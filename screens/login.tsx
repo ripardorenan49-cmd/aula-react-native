7367ac5e673bf520f12a45d61a7b42f7774ca1b4
lucas erick <lucaserickmendestavares@gmail.com>
2025-11-27 19:48:15 -0300

    feat: add area util

---
+++ screens/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function LoginScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-amber-300">
+      <Text className="text-6xl">tela de login</Text>
+    </View>
+  );
+}
