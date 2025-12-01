f40a92eb817d69a67fd454835b6b436e361b0920
lucas erick <lucaserickmendestavares@gmail.com>
2025-12-01 20:37:26 -0300

    feature: add campo esqueceu a senha

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -5,9 +5,18 @@ import { Text, View } from 'react-native';
 export function LoginScreen() {
   return (
     <View className="flex-1 items-center justify-center">
+      <View className="w-full p-5 gap-5 justify-center items-center">
+        <Text className="text-3xl">Nosso app</Text>
+        
       <InputCustomizado placeholder={'Login'} />
       <InputCustomizado placeholder="Senha" />
       <BotaoCustomizado title="entrar" />
+
+      <Text className="text-blue-300">
+        Esqueceu a senha?
+      </Text>
+
+      </View>
     </View>
   );
 }
