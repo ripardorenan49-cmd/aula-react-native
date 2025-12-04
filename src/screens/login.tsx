6c7efb81c2f7b8fafe8188b4c1287475bdb18899
lucas erick <lucaserickmendestavares@gmail.com>
2025-12-04 18:45:57 -0300

    feature: style

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -4,31 +4,36 @@ import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
-  const[login, setLogin] = useState ('');
-  const[senha, setSenha] = useState (''); 
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function logar() {
+    console.log('login:', login, 'senha:', senha);
+  }
+
   return (
     <View className="flex-1 items-center justify-center">
-      <View className="w-full p-5 gap-5 justify-center items-center">
+      <View className="w-full items-center justify-center gap-5 p-5">
         <Text className="text-3xl">Nosso app</Text>
-        
-      <InputCustomizado placeholder={'Login'}
-      onChangeText={(Text) => setLogin(Text)}
-      />
-      <InputCustomizado placeholder="Senha" 
-      onChangeText={(Text) => setSenha(Text)}
-      secureTextEntry={true}
-      />
-    
-      <BotaoCustomizado title="entrar"
-      onPress={() => console.log("login:", login, "senha:", senha)}
-      />
-      
-      
-      <Text className="text-blue-300">
-        Esqueceu a senha?
-      </Text> 
 
+        <InputCustomizado placeholder={'Login'} onChangeText={(Text) => setLogin(Text)} />
+        <InputCustomizado
+          placeholder="Senha"
+          onChangeText={(Text) => setSenha(Text)}
+          secureTextEntry={true}
+        />
+
+        <BotaoCustomizado title="entrar" onPress={logar} />
+        <View className="flex-row gap-4">
+          <Text className="text-blue-500">cadastra-se</Text>
+
+          <Text className="text-xl">|</Text>
+
+          <Text className="text-blue-500">Esqueceu a senha?</Text>
+        </View>
+
+        <Text className="text-blue-300">Esqueceu a senha?</Text>
       </View>
     </View>
   );
-} 
+}
